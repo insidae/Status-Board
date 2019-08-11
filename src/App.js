c62e31db7e37f382d28e5116f58964c959fa7c5f
insidae <insidae@gmail.com>
2019-08-11 13:10:18 +0100

    Adding weather icons and basic time component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Time from "./components/Time";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Time</h2>
+          <Time></Time>
           <h2>Date</h2>
           <h2>Weather</h2>
           <h2>Today</h2>
---
+++ src/components/Time.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Time extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        time: new Date().toLocaleString()
+      };
+    }
+    componentDidMount() {
+      this.intervalID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+    componentWillUnmount() {
+      clearInterval(this.intervalID);
+    }
+    tick() {
+      this.setState({
+        time: new Date().toLocaleString()
+      });
+    }
+    render() {
+      return (
+        <p className="App-time">
+          {this.state.time}
+        </p>
+      );
+    }
+  }
+
+export default Time;
