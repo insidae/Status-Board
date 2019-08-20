600e24628706a594705ab17fc02d303cb7a7f28b
insidae <insidae@gmail.com>
2019-08-20 12:06:19 +0100

    Updating am/pm code so it can be styled separately to 'time'

--- src/components/TimeDate.js
+++ src/components/TimeDate.js
@@ -8,7 +8,8 @@ class TimeContainer extends Component {
         day: d.getDay(),
         month: d.getMonth(),
         date: d.getDate(),
-        time: d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true})
+        time: d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false}),
+        ampm: d.getHours() >= 12 ? 'PM' : 'AM'
       }
     this.countingSecond = this.countingSecond.bind(this)
   }
@@ -18,10 +19,11 @@ class TimeContainer extends Component {
       day: d.getDay(),
       month: d.getMonth(),
       date: d.getDate(),
-      time: d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true})
+      time: d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false}),
+      ampm: d.getHours() >= 12 ? 'pm' : 'am'
     })
   }
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     setInterval(this.countingSecond, 1000)
   }
   render() {
@@ -29,7 +31,7 @@ class TimeContainer extends Component {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   return (
       <div className='time-date section'>
-        <h1 className='time'>{this.state.time}</h1>
+        <h1 className='time'>{this.state.time}<span className='ampm'>{this.state.ampm}</span></h1>
         <h2 className='date'>{days[this.state.day]}, {months[this.state.month]} {this.state.date}</h2>
       </div>
     )
